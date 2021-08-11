672cccd4a48993c7d95fc7a67984c44c7566b297
David Granados <israeldavi0904@gmail.com>
2021-08-11 17:22:10 -0500

    suma de los numeros positivos

---
+++ datos_simples_6.py
@@ -0,0 +1,4 @@
+#Escribir un programa que lea un entero positivo,n, introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta n. La suma de los n primeros enteros positivos puede ser calculada de la siguiente forma:n(n+1)/2
+numero=int(input("Introduce un número: "))
+suma=(numero*(numero+1))/2
+print(suma)
